8c15ff557d48a33a0850eb79a16a990bf0bd9d74
sathish55512 <131868785+sathish55512@users.noreply.github.com>
2023-07-18 18:38:54 +0530

    Add files via upload

---
+++ INVENTORY MANAGEMENT/inventory_managementSQL.sql
@@ -0,0 +1,27 @@
+create database inventory_management;
+use inventory_management;
+create table product (
+product_id varchar(50) not null,
+primary key (product_id)
+);
+create table location(
+location_id varchar(50) not null,
+primary key (location_id)
+);
+create table productManagement(
+from_location varchar(50),
+to_location varchar(50),
+product_id varchar(50) not null,
+qty int not null
+);
+
+create table report(
+location_id varchar(50) not null ,
+product_id varchar(50) not null,
+qty int not null
+);
+
+select *from product;
+select *from location;
+select *from report;
+
